49dde8f47c682d6509c82d38c0ad9dda14566648
Bohdan Fedorov <Bohdan_Fedorov@epam.com>
2021-10-13 16:59:26 +0300

    archiveMyBuild file zip change

--- vars/archiveMyBuild.groovy
+++ vars/archiveMyBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
-    sh 'sudo zip target/master-demo-app-${OWNER}-${BUILD_NUMBER}.zip target/*.jar'
+    sh 'sudo zip target/master-demo-app-${OWNER}-${BUILD_NUMBER}.zip'
     archiveArtifacts artifacts: 'target/*.zip', fingerprint: true
     echo 'Archived successful'
 }
