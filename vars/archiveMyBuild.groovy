67b51ff2270e19203c6d16b0ab11327ab25f53ff
Bohdan Fedorov <Bohdan_Fedorov@epam.com>
2021-10-13 17:32:04 +0300

    'target/*.zip'

--- vars/archiveMyBuild.groovy
+++ vars/archiveMyBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
-    sh 'sudo zip build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION:latest'
-    archiveArtifacts artifacts: 'build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION.zip', fingerprint: true
+    sh 'sudo zip target/build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION.zip target/*.jar '
+    archiveArtifacts artifacts: 'target/*.zip', fingerprint: true
     echo 'Archived successfully'
 }
