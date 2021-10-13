a8905864d8236cced178e3dba5c045ad61dc1fa3
Bohdan Fedorov <Bohdan_Fedorov@epam.com>
2021-10-13 17:22:33 +0300

    sudo zip build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION:latest add

--- vars/archiveMyBuild.groovy
+++ vars/archiveMyBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
-    sh 'sudo zip build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION'
+    sh 'sudo zip build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION:latest'
     archiveArtifacts artifacts: 'build-$BUILD_NUMBER-on-docker-version-$DOCKER_VERSION.zip', fingerprint: true
     echo 'Archived successfully'
 }
