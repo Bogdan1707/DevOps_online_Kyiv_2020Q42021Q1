3af7eb7ca04d70dc5f84ba7cab8fee34312d5dd9
Bohdan Fedorov <Bohdan_Fedorov@epam.com>
2021-10-13 18:20:31 +0300

    /home/bbf236579/workspace/pipeline2_master

--- vars/archiveMyBuild.groovy
+++ vars/archiveMyBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
-    sh 'sudo zip pipeline2/master/build-${BUILD_NUMBER}-on-docker-version-$DOCKER_VERSION'
-    archiveArtifacts artifacts: 'pipeline2/master/*.zip', fingerprint: true
+    sh 'sudo zip /home/bbf236579/workspace/pipeline2_master/build-${BUILD_NUMBER}-on-docker-version-$DOCKER_VERSION'
+    archiveArtifacts artifacts: '/home/bbf236579/workspace/pipeline2_master/*.zip', fingerprint: true
     echo 'Archived successfully'
 }
